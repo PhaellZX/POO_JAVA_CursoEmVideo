77613da45d8cbb950552a651a3713bd9568b37dc
Raphael <48337836+PhaellZX@users.noreply.github.com>
2021-05-06 13:25:04 -0300

    Add files via upload

---
+++ Biblioteca/src/classes/Main.java
@@ -0,0 +1,24 @@
+package classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Pessoa[] p = new Pessoa[3];
+		Livro[] l = new Livro[4];
+		
+		p[0] = new Pessoa("Pedro", 22, "M");
+		p[1] = new Pessoa("Maria", 25, "F");
+		
+		l[0] = new Livro("Apredendo Java","José da Silva",300,p[0]);
+		l[1] = new Livro("POO para Iniciantes","Pedro Paulo",500,p[1]);
+		l[2] = new Livro("Java Avançado","Maria Candido",800,p[0]);
+		
+		l[0].abrir();
+		l[0].folhear(200);
+		l[0].avancarPag();
+		
+		System.out.println(l[0].detalhes());
+		
+		System.out.println(l[1].detalhes());
+	}
+}
